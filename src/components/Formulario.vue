c6c65a2102502991a4bbee365b9d28be095b21bf
Angela Ribeiro <angelaribeiro2@hotmail.com>
2023-02-22 09:50:05 -0300

    form/temp

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -6,23 +6,7 @@
             </div>
 
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-
-                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fa-solid fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+              
 
 
             </div>
@@ -33,37 +17,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Cronometro from './Cronometro.vue'
+import Temporizador from './Temporizador.vue'
+
 export default defineComponent({
     name: 'Formulario',
     components: {
-        Cronometro
-    },
-
-
-    data() {
-        return {
-            tempoEmSegundos: 0,
-            cronometro: 0
-        }
-    },
-
-
-
-    methods: {
-        iniciar() {
-            //começar a contagem 
-            // 1 segundo = 1000 milisegundos
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1
-            }, 1000);
-        },
-        finalizar() {
-            clearInterval(this.cronometro);
-        }
+        Temporizador
     }
-});
-
 
 </script>
 
